adcaa88cb16fcf746b85dd1e5b7a1ee396ae208e
Rakhis Yudha <rakhisyuda@gmail.com>
2024-09-26 19:43:18 +0700

    learn

--- hello.go
+++ hello.go
@@ -3,8 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	// simple print
 	fmt.Println("Hello World")
 	fmt.Println("Satu = ", 1)
 	fmt.Println("Dua = ", 2)
-	fmt.Println("Tiga = ", 3)
+	//float print
+	fmt.Println("Tiga koma lima = ", 3.5)
+	//boolean print
+	fmt.Println("Benar = ", true)
+	fmt.Println("Salah =", false)
+	//string print
+	fmt.Println(len("Rakhis"))
+	fmt.Println("Rakhis"[0])
 }
